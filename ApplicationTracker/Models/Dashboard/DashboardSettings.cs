57e114ddbd390a89e94126679626be2b17ecc6a5
hgrzes2411 <135008021+hgrzes2411@users.noreply.github.com>
2026-07-30 14:15:26 +0200

    UI Refactor

---
+++ ApplicationTracker/Models/Dashboard/DashboardSettings.cs
@@ -0,0 +1,24 @@
+﻿namespace ApplicationTracker.Models.Dashboard;
+
+public class DashboardSettings
+{
+    public bool ShowSummary { get; set; } = true;
+
+    public bool ShowCharts { get; set; } = true;
+
+    public bool ShowCompanyAnalytics { get; set; } = true;
+
+    public bool ShowCompanyInsights { get; set; } = true;
+
+    public bool ShowRecommendations { get; set; } = true;
+
+    public bool ShowResponseAnalytics { get; set; } = true;
+
+    public bool ShowSourceAnalytics { get; set; } = true;
+
+    public bool ShowRecentActivity { get; set; } = true;
+
+    public bool ShowSuccessRate { get; set; } = true;
+
+    public bool ShowRecruitmentFunnel { get; set; } = true;
+}
